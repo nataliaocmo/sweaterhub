92b80008c4db8049d6d6c0637f958e3521d57127
nataliaocmo <nataliaocmo@unisabana.edu.co>
2024-11-26 15:41:46 -0500

    imagePicker

--- app/pages/create/index.tsx
+++ app/pages/create/index.tsx
@@ -1,14 +1,12 @@
-import {
-  View,
-  Image,
-  StyleSheet,Text,
-  TouchableOpacity
-} from 'react-native';
-import React, { useEffect, useState } from 'react';
+import {View,StyleSheet,Text,TouchableOpacity, Image} from 'react-native';
+import React, { useEffect, useState, } from 'react';
 import Background from '@/components/background';
 import Topbar from '@/components/topbar';
 import Menu from '@/components/menu';
 import { useLocalSearchParams } from 'expo-router';
+import FontAwesome from '@expo/vector-icons/FontAwesome';
+import * as ImagePicker from 'expo-image-picker';
+
 
 export default function Index() {
 
@@ -18,6 +16,7 @@ export default function Index() {
  
   const [isDrawer, setIsDrawer] = useState(false);
   const closeMenu = () => setIsDrawer(false);
+  const [currentPhoto, setCurrentPhoto] = useState(undefined as any);
 
    // Confirmar que el componente se monta
    useEffect(() => {
@@ -27,6 +26,22 @@ export default function Index() {
   console.log('Parámetros recibidos:', useLocalSearchParams());
   console.log('URL de la imagen recibida:', imageUrl);
 
+ 
+  const pickImage = async () => {
+    // No permissions request is necessary for launching the image library
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ['images', 'videos'],
+      allowsEditing: true,
+      aspect: [4, 3],
+      quality: 1,
+    });
+
+    console.log(result);
+
+    if (!result.canceled) {
+      setCurrentPhoto(result.assets[0].uri);
+    }
+  };
  
   return (
     <>
@@ -38,7 +53,10 @@ export default function Index() {
 
         <TouchableOpacity
         style={[styles.img]}
+        onPress={pickImage}
       >
+        {currentPhoto && <Image source={{ uri: currentPhoto }} style={styles.image} />}
+      
         <FontAwesome name="photo" size={24} color="white" />
       </TouchableOpacity>
 
@@ -68,5 +86,12 @@ const styles = StyleSheet.create({
   image: {
     width: 300, // Ancho de la imagen
     height: 300, // Altura de la imagen
+  },
+  img: {
+    position: 'absolute', // Posiciona el botón de foto encima
+    top: 20, // Colócalo un poco abajo desde la parte superior
+    left: '50%', // Centra el botón horizontalmente
+    transform: [{ translateX: -20 }], // Ajusta el botón para que esté perfectamente centrado
+    zIndex: 10, // Asegura que esté encima de otros elementos
   },
 });
